26485de1f99ee44c8585526ebfd28b0bec61c0de
jfedericobn <jesus@blindsidenetworks.com>
2025-10-29 20:58:15 +0000

    added backup/restore

---
+++ backup/moodle2/backup_bbbext_bnx_subplugin.class.php
@@ -0,0 +1,60 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Backup support for the BN Experience subplugin.
+ *
+ * @package   bbbext_bnx
+ * @copyright 2025 onwards, Blindside Networks Inc
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @author    Jesus Federico  (jesus [at] blindsidenetworks [dt] com)
+ */
+class backup_bbbext_bnx_subplugin extends backup_subplugin {
+    /**
+     * Append the BN Experience data to the activity structure.
+     *
+     * @return backup_subplugin_element
+     */
+    protected function define_bigbluebuttonbn_subplugin_structure() {
+        $subplugin = $this->get_subplugin_element();
+        $wrapper = new backup_nested_element($this->get_recommended_name());
+
+        $bnxelement = new backup_nested_element('bbbext_bnx', ['id'], [
+            'timecreated',
+            'timemodified',
+        ]);
+
+        $settings = new backup_nested_element('bbbext_bnxsettings', ['id'], [
+            'setting',
+            'value',
+            'timemodified',
+        ]);
+
+        $subplugin->add_child($wrapper);
+        $wrapper->add_child($bnxelement);
+        $bnxelement->add_child($settings);
+
+        $bnxelement->set_source_table('bbbext_bnx', [
+            'bigbluebuttonbnid' => backup::VAR_PARENTID,
+        ]);
+
+        $settings->set_source_table('bbbext_bnxsettings', [
+            'bnxid' => backup::VAR_PARENTID,
+        ]);
+
+        return $subplugin;
+    }
+}
---
+++ backup/moodle2/restore_bbbext_bnx_subplugin.class.php
@@ -0,0 +1,92 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Restore support for the BN Experience subplugin.
+ *
+ * @package   bbbext_bnx
+ * @copyright 2025 onwards, Blindside Networks Inc
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @author    Jesus Federico  (jesus [at] blindsidenetworks [dt] com)
+ */
+class restore_bbbext_bnx_subplugin extends restore_subplugin {
+    /**
+     * Declare the paths handled by this subplugin during restore.
+     *
+     * @return restore_path_element[]
+     */
+    protected function define_bigbluebuttonbn_subplugin_structure() {
+        $paths = [];
+
+        $paths[] = new restore_path_element(
+            $this->get_namefor('bnx'),
+            $this->get_pathfor('/bbbext_bnx')
+        );
+
+        $paths[] = new restore_path_element(
+            $this->get_namefor('bnxsetting'),
+            $this->get_pathfor('/bbbext_bnx/bbbext_bnxsettings')
+        );
+
+        return $paths;
+    }
+
+    /**
+     * Persist the BN Experience record for a restored activity.
+     *
+     * @param array $data
+     */
+    public function process_bbbext_bnx_bigbluebuttonbn($data) {
+        global $DB;
+
+        $data = (object) $data;
+        $data->bigbluebuttonbnid = $this->get_new_parentid('bigbluebuttonbn');
+
+        if (empty($data->timecreated)) {
+            $data->timecreated = time();
+        }
+
+        if (empty($data->timemodified)) {
+            $data->timemodified = $data->timecreated;
+        }
+
+        $newid = $DB->insert_record('bbbext_bnx', $data);
+        $this->set_mapping('bbbext_bnx', $data->id, $newid);
+    }
+
+    /**
+     * Persist the BN Experience setting records for a restored activity.
+     *
+     * @param array $data
+     */
+    public function process_bbbext_bnxsetting_bigbluebuttonbn($data) {
+        global $DB;
+
+        $data = (object) $data;
+        $data->bnxid = $this->get_mappingid('bbbext_bnx', $data->bnxid);
+
+        if (empty($data->bnxid)) {
+            // Parent record not restored (unlikely but safe guard).
+            return;
+        }
+
+        if (empty($data->timemodified)) {
+            $data->timemodified = time();
+        }
+
+        $DB->insert_record('bbbext_bnxsettings', $data);
+    }
+}
